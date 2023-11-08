331147065313538c0f57bdf8f9bc3ecc878821fd
dom.song <dom.song@amperecomputing.com>
2023-11-08 02:35:19 -0800

    Capture log when call the script

--- all_spec2017.sh
+++ all_spec2017.sh
@@ -6,6 +6,6 @@ if [ $# -lt 3 ];then
     exit 0
 fi
 
-./host_spec2017.sh log_dir $LOG_DIR $@ 2>&1 > $LOG_DIR/run.log
-./qemu_spec2017.sh log_dir $LOG_DIR $@ 2>&1 >> $LOG_DIR/run.log
-./ch_spec2017.sh log_dir $LOG_DIR $@ 2>&1 >> $LOG_DIR/run.log
+./host_spec2017.sh log_dir $LOG_DIR $@ #2>&1 > $LOG_DIR/run.log
+./qemu_spec2017.sh log_dir $LOG_DIR $@ #2>&1 >> $LOG_DIR/run.log
+./ch_spec2017.sh log_dir $LOG_DIR $@ #2>&1 >> $LOG_DIR/run.log
