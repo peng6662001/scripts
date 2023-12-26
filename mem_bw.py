1d8f46c2ba7716f2cc0f5f6100e1bc2029f3d573
dom.song <dom.song@amperecomputing.com>
2023-12-26 14:38:03 +0800

    perf for ampere_mcu_pmu

---
+++ mem_bw.py
@@ -0,0 +1,52 @@
+import csv, sys, os
+import re
+
+csv_data = []
+perf_data = {}
+
+file_name = sys.argv[1]
+duration = int(sys.argv[2])
+
+if duration < 1:
+    duration = 1
+
+csv_file = open(file_name)
+reader_obj = csv.reader(csv_file)
+csv_data = list(reader_obj)
+
+for row in csv_data:
+    if (len(row) <= 1):
+        continue
+    a = perf_data.get(row[3], {}) # Note, get() returns a value not ref
+    a['val'] = a.get('val', 0) + float(row[1])
+    perf_data[row[3]] = a # adam: we have to assign it back to dict
+
+pattern_num = r'\d+'
+wr_bw_s0 = 0
+wr_bw_s1 = 0
+rd_bw_s0 = 0
+rd_bw_s1 = 0
+
+for k in perf_data.keys():
+    num = int(re.findall(pattern_num, k)[0])
+    if 'wr_wra_sent' in str(k):
+        if num < 16:
+            wr_bw_s0 += perf_data[k].get('val')
+        else:
+            wr_bw_s1 += perf_data[k].get('val')
+    if 'rd_rda_sent' in str(k):
+        if num < 16:
+            rd_bw_s0 += perf_data[k].get('val')
+        else:
+            rd_bw_s1 += perf_data[k].get('val')
+
+wr_bw_s0 = wr_bw_s0*64/1024/1024/duration
+wr_bw_s1 = wr_bw_s1*64/1024/1024/duration
+rd_bw_s0 = rd_bw_s0*64/1024/1024/duration
+rd_bw_s1 = rd_bw_s1*64/1024/1024/duration
+
+
+print(f"rd_bw_s0: {rd_bw_s0:.2f} MiBs")
+print(f"rd_bw_s1: {rd_bw_s1:.2f} MiBs")
+print(f"wr_bw_s0: {wr_bw_s0:.2f} MiBs")
+print(f"wr_bw_s1: {wr_bw_s1:.2f} MiBs")
---
+++ pmu_mem_bw.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+if [ $# -lt 3 ];then
+    echo "usage:./pmu_mem_bw.sh 1"
+    exit 1
+fi
+
+duration=$1
+
+wr_cmd="ampere_mcu_pmu_0/wr_wra_sent/"
+rd_cmd="ampere_mcu_pmu_0/rd_rda_sent/"
+for i in $(seq 1 31)
+do
+	wr_cmd="$wr_cmd,ampere_mcu_pmu_$i/wr_wra_sent/"
+	rd_cmd="$rd_cmd,ampere_mcu_pmu_$i/rd_rda_sent/"
+done
+
+while [ 1 ]
+do
+	perf stat -I 1000 -e $wr_cmd,$rd_cmd -o mem_bw.log -x, -- sleep $duration
+	python ./mem_bw.py mem_bw.log $duration
+	sleep 1
+done
+
